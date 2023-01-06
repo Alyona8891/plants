f957f43d059964ba863d8bfe465f377474d53a5f
Alyona Shupenyova <besssta888@mail.ru>
2023-01-07 00:51:59 +0300

    feat: add menu-burger

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,1 +1,18 @@
 console.log('Score: 100 /100 \n 1.Вёрстка валидная +10 \n 2.Вёрстка семантическая +20 \n 3.Вёрстка соответствует макету +48 \n 4.Требования к css + 12 \n 5.Интерактивность, реализуемая через css +20');
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+ hamb.addEventListener("click", hambHandler);
+
+ function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+ }
+ function renderPopup() {
+    popup.appendChild(menu);
+ }
