4ad36eb26d83b83ea6cc159f6d6db3aa09d368f4
Alyona Shupenyova <besssta888@mail.ru>
2023-01-19 14:10:13 +0300

    fix: correct project according to pp

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,13 +1,13 @@
-console.log('Score: 100 /100 \n 1.Вёрстка валидная +10 \n 2.Вёрстка семантическая +20 \n 3.Вёрстка соответствует макету +48 \n 4.Требования к css + 12 \n 5.Интерактивность, реализуемая через css +20');
+console.log('Score: 75 /75 \n 1.Вёрстка соответствует макету. Ширина экрана 768px +24 \n 2.Вёрстка соответствует макету. Ширина экрана 380px +24 \n 3.Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15 \n 4.На ширине экрана 380рх и меньше реализовано адаптивное меню +22');
 const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
 const menu = document.querySelector("#menu").cloneNode(1);
 const body = document.body;
 
  hamb.addEventListener("click", hambHandler);
-
  function hambHandler(e) {
     e.preventDefault();
+    e.stopPropagation();
     popup.classList.toggle("open");
     hamb.classList.toggle("active");
     body.classList.toggle("noscroll");
@@ -23,4 +23,10 @@ const body = document.body;
    body.classList.toggle("noscroll");
    hamb.classList.toggle("active");
  }
- 
+document.onclick = function (e) {
+   if (e.className != 'active') {
+      popup.classList.remove("open");
+      hamb.classList.remove("active");
+      body.classList.remove("noscroll");
+   }
+}
